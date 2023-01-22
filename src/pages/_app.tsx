a4836ee5090a394e593fc93cc89251f3b981287d
lohit244 <lohit244@gmail.com>
2023-01-22 12:24:38 +0530

    day 2 next-progress

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,12 @@
 import Navbar from '@/components/navbar'
 import '@/styles/globals.css'
+import NextProgress from 'next-progress'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
 
   return <>
-
+    <NextProgress />
     <Navbar />
     <Component {...pageProps} />
 
