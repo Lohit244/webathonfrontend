386cd12050db645f86faf3493ab69b437f571391
lohit244 <lohit244@gmail.com>
2023-01-22 16:23:44 +0530

    mopio

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -16,15 +16,30 @@ const Navbar = () => {
   });
   return (
     <>
-      <div className="flex flex-row justify-end md:mx-16 py-2 sm:mx-8">
+      <div className="flex flex-col-reverse justify-end md:mx-16 py-2 sm:mx-8 sm:flex-row gap-2">
         <div>
         </div>
         {loggedIn && (
-          <Link
-            href="/myposts"
-            className="justify-center flex bg-blue-500 mx-3  hover:bg-blue-600 transition-colors duration-300 rounded-sm px-[2ch] items-center">
-            My Postings
-          </Link>
+          <>
+            <Link
+              href="/myposts"
+              className="justify-center flex bg-blue-500 hover:bg-blue-600 transition-colors duration-300 rounded-sm px-[2ch] py-1 items-center">
+              My Postings
+            </Link>
+
+            <Link
+              href="/partof"
+              className="justify-center flex bg-violet-500 hover:bg-violet-600 transition-colors duration-300 rounded-sm px-[2ch] py-1 items-center">
+              Accepted Applications
+            </Link>
+
+            <Link
+              href="/create-event"
+              className="justify-center flex bg-green-700 hover:bg-green-800 transition-colors duration-300 rounded-sm px-[2ch] py-1 items-center">
+              Post an event
+            </Link>
+
+          </>
         )}
         <button
           className={`${loggedIn ? "bg-red-500" : "bg-blue-500"} ${loggedIn ? "hover:bg-red-600" : "hover:bg-blue-600"
